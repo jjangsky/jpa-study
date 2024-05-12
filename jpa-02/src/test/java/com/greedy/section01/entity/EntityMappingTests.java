0e9f2007eca49ecc94c8c97c4158e9fc40418aec
YouchanKim <dbcks4646@gmail.com>
2024-05-13 01:00:39 +0900

    테이블 생성 및 데이터 등록 테스트 코드 작성

--- jpa-02/src/test/java/com/greedy/section01/entity/EntityMappingTests.java
+++ jpa-02/src/test/java/com/greedy/section01/entity/EntityMappingTests.java
@@ -1,4 +1,70 @@
 package com.greedy.section01.entity;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class EntityMappingTests {
+
+    private static EntityManagerFactory entityManagerFactory;
+
+    private EntityManager entityManager;
+
+    @BeforeAll
+    public static void initFactiory(){
+        // 환경 생성
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
+
+    }
+
+    @BeforeEach
+    public void initManager(){
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+
+
+    @AfterAll
+    public static void closeFactory(){
+        // 환경 종료
+        entityManagerFactory.close();
+    }
+
+    @AfterEach
+    public void closeManager(){
+        // 엔티티 매니저 종료
+        entityManager.close();
+    }
+
+    @Test
+    public void create_test_table() {
+       // 테이블 만들기 테스트
+
+        // given
+        Member member = new Member();
+        member.setMemberNo(1);
+        member.setMemberId("user01");
+        member.setMemberPwd("pass01");
+        member.setNickName("사용자01");
+        member.setPhone("010-1234-5678");
+        member.setEmail("test@naver,com");
+        member.setAddress("서울시 강남구 역삼동");
+        member.setEnrollDate(new Date(System.currentTimeMillis()));
+        member.setMembberRole("user");
+        member.setStatus("Y");
+
+        // when
+        entityManager.persist(member);
+
+        // then
+        Member foundMember = entityManager.find(Member.class, 1);
+        assertEquals(member.getMemberNo(), foundMember);
+
+    }
+
 }
--- jpa-02/src/test/java/com/greedy/section01/entity/Member.java
+++ jpa-02/src/test/java/com/greedy/section01/entity/Member.java
@@ -31,5 +31,83 @@ public class Member {
     @Column(name = "STATUS")
     private String status;
 
+    public int getMemberNo() {
+        return memberNo;
+    }
 
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public String getMemberPwd() {
+        return memberPwd;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Date getEnrollDate() {
+        return enrollDate;
+    }
+
+    public String getMembberRole() {
+        return membberRole;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setMemberNo(int memberNo) {
+        this.memberNo = memberNo;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public void setMemberPwd(String memberPwd) {
+        this.memberPwd = memberPwd;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setEnrollDate(Date enrollDate) {
+        this.enrollDate = enrollDate;
+    }
+
+    public void setMembberRole(String membberRole) {
+        this.membberRole = membberRole;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
