82cc71112defa649116f82b8deb00fb0cd21c35f
YouchanKim <dbcks4646@gmail.com>
2024-05-19 23:13:21 +0900

    jpa04- jpql Query 방식 작성

--- jpa-03/src/test/java/com/greedy/section01/manytoone/ManyToOneAssociationTests.java
+++ jpa-03/src/test/java/com/greedy/section01/manytoone/ManyToOneAssociationTests.java
@@ -40,25 +40,7 @@ public class ManyToOneAssociationTests {
         // 엔티티 매니저 종료
         entityManager.close();
     }
-    /*
-     * Association Mapping은 Entity 클래스 간의 관계를 매핑하는 것을 의미함(Join을 위함)
-     * 이를 통해 겍체를 이용해 데잍터베이스의 테이블 간의 관계를 활용해 한번에 객체로 조회할 수 있다.
-     *
-     * 1. 다중성에 의한 분류
-     * : 연관 관계가 있는 객체 관계에서 실제로 연관을 가지는 객체의 수에 따라 분류한다.
-     *  - N:1(Many To One) : 다대일 관계
-     * - 1:N(One To Many) : 일대다 관계
-     * - 1:1(One To One) : 일대일 관계
-     * - N:M(Many To Many) : 다대다 관계
-     *
-     * 2. 방향에 따른 분류
-     * : 테이블의 연관 관계는 외래 키를 이용하여 양방향 연관 관계의 특징을 가진다.
-     * 반면, 객체는 참조에 의한 연관관계로 단방향이다.
-     * 객체 간의 연관 관계를 양방향으로 만들고 싶을 경우 반대 쪽에서도 필드를 추가해서 참조를
-     * 보관하면 된다. 하지만 엄밀하게 양방향 관계가 아니라 단방향 관계를 2개로 볼 수 있다.
-     * - 단방향(Unidirectional) : 한 쪽에서만 참조를 보관
-     * - 양방향(Bidirectional) : 양쪽에서 서로 참조를 보관
-     */
+
     @Test
     public void manyToOneObjectMappingTest() {
         // 객체 매핑 테스트
--- jpa04/src/test/java/com/greedy/section01/simple/SimpleJPQLTests.java
+++ jpa04/src/test/java/com/greedy/section01/simple/SimpleJPQLTests.java
@@ -1,9 +1,6 @@
 package com.greedy.section01.simple;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-import jakarta.persistence.TypedQuery;
+import jakarta.persistence.*;
 import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -40,12 +37,41 @@ public class SimpleJPQLTests {
         entityManager.close();
     }
 
-    @Test // 단일행 단일열 조회 테스트
+    /**
+     *  [JPQL]
+     *  - SELECT, UPDATE, DELETE 등의 키워드 사용은 SQL과 동일하다
+     *  - INSERT는 persist() 메소드를 사용한다.
+     *  - 키워드는 대소문자를 구분하지 않지만, 엔티티와 속성은 대소문자를 구분하므로 유의한다.
+     *  - 엔티티 별칭을 필수로 사용해야 하며 별칭 없이 작성하면 에러가 발생한다.
+     */
+
+    /**
+     * jpql 사용 방법
+     * 1. 작성한 jpql(문자열)을 `entityManager.createQuery()` 메소드를 이용해 Query 객체로 생성한다.
+     * 2. 쿼리 객체는 `TypedQuery`와 `Query` 두 가지가 있다.
+     *      2-1. TypedQuery : 반환할 타입을명확하게 지칭하는 방식일 때 사용하며 쿼리 객체의 메소드 실행결과로 지정한 타입이 반환된다.
+     *      2-2. Query : 반환할 타입을 명확하게 지정할 수 없을 때 사용하며 쿼리 객체 메소드의 실행 결과로 Object 또는 Objectp[]이 반환된다.
+*    * 3. 쿼리 객체에서 제공하는 메소드를 호출해서 쿼리를 실행하고 데이터베이스를 조회한다.
+     *      3-1. getSingleResult(): 결과가 정확히 한 행일 경우 사용하며 없거나 많으면 예외가 발생한다.
+     *      3-2. getResultList(): 결과가 2행 이상일 경우 사용하며 켈력션을 반환한다. 없으면 빈 컬렉션 반환
+     */
+    @Test // 단일행 단일열 조회 테스트 - TypedQuery
     public void TypeQueryTest() {
         String jpql = "SELECT m FROM menu m where m.menuCode = 7";
         TypedQuery<String> query = entityManager.createQuery(jpql, String.class);
 
         String resultMenuName = query.getSingleResult();
+        System.out.println("resultMenuName : " + resultMenuName);
+
+        assertEquals("치즈볼", resultMenuName);
+    }
+
+    @Test // 단일행 단일열 조회 테스트 - Query
+    public void QueryTest() {
+        String jpql = "SELECT m.menuName FROM menu m where m.menuCode = 7";
+        Query query = entityManager.createQuery(jpql);
+
+        String resultMenuName = (String) query.getSingleResult();
         System.out.println("resultMenuName : " + resultMenuName);
 
         assertEquals("치즈볼", resultMenuName);
